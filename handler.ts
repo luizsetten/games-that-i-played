6b838950f3bed1f772b7d069f5e9355171a3f074
Luiz Gustavo Chinelato Setten <luizgustavosetten@hotmail.com>
2024-10-17 14:45:53 -0300

    refactor: change project structure

--- handler.ts
+++ handler.ts
@@ -1,53 +1,5 @@
-const { DynamoDBClient } = require("@aws-sdk/client-dynamodb");
+import { createGameLambda } from "./routes/games/create";
+import { listGamesLambda } from "./routes/games/list";
 
-const {
-  DynamoDBDocumentClient,
-  GetCommand,
-  PutCommand,
-} = require("@aws-sdk/lib-dynamodb");
-
-const express = require("express");
-const serverless = require("serverless-http");
-
-const app = express();
-
-const GAMES_TABLE = process.env.GAMES_TABLE;
-const client = new DynamoDBClient();
-const docClient = DynamoDBDocumentClient.from(client);
-
-app.use(express.json());
-
-app.get("/games/:gameId", async (req, res) => {
-  const params = {
-    TableName: GAMES_TABLE,
-    Key: {
-      gameId: req.params.gameId,
-    },
-  };
-
-  try {
-    const command = new GetCommand(params);
-    const { Item } = await docClient.send(command);
-    if (Item) {
-      const { gameId, name } = Item;
-      res.json({ gameId, name });
-    } else {
-      res
-        .status(404)
-        .json({ error: 'Could not find user with provided "gameId"' });
-    }
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({ error: "Could not retrieve user" });
-  }
-});
-
-app.post("/games", );
-
-app.use((req, res, next) => {
-  return res.status(404).json({
-    error: "Not Found",
-  });
-});
-
-exports.handler = serverless(app);
+module.exports.create = createGameLambda;
+module.exports.list = listGamesLambda;
--- routes/games/create.ts
+++ routes/games/create.ts
@@ -1,25 +1,60 @@
-import { PutCommand } from "@aws-sdk/lib-dynamodb";
+import { DynamoDBClient, PutItemCommand } from "@aws-sdk/client-dynamodb";
+import { DynamoDBDocumentClient, GetCommand, PutCommand } from "@aws-sdk/lib-dynamodb";
+import { LambdaFunctionURLHandler } from "aws-lambda";
+import { v4 } from "uuid";
 
-export const routeCreate = 
-  async (req, res) => {
-    const { gameId, name } = req.body;
-    if (typeof gameId !== "string") {
-      res.status(400).json({ error: '"gameId" must be a string' });
-    } else if (typeof name !== "string") {
-      res.status(400).json({ error: '"name" must be a string' });
-    }
-  
-    const params = {
-      TableName: GAMES_TABLE,
-      Item: { gameId, name },
-    };
-  
-    try {
-      const command = new PutCommand(params);
-      await docClient.send(command);
-      res.json({ gameId, name });
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ error: "Could not create user" });
+const GAMES_TABLE = process.env.GAMES_TABLE;
+const client = new DynamoDBClient();
+const docClient = DynamoDBDocumentClient.from(client);
+
+export const createGameLambda: LambdaFunctionURLHandler = async (event, context, callback) => {
+  if (!event.body) return { statusCode: 400 }
+
+  const body = JSON.parse(event.body);
+
+  const { name } = body;
+  if (typeof name !== "string") {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({ error: "Missing 'name' in request body" }),
     }
   }
+
+
+  try {
+    const { Item } = await docClient.send(new GetCommand({
+      Key: {
+        name
+      }, TableName: GAMES_TABLE
+    }));
+
+    if (Item) {
+      return {
+        statusCode: 409,
+        body: JSON.stringify({ error: "Game with the same name already exists" })
+      }
+    }
+
+    const id = v4();
+    const params = {
+      TableName: GAMES_TABLE,
+      Item: { gameId: id, name },
+    };
+
+    const command = new PutCommand(params);
+    await docClient.send(command);
+
+    return {
+      statusCode: 201,
+      body: JSON.stringify({ id })
+    }
+  } catch (error) {
+    console.error(error);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ message: "Could not create game", error })
+    }
+  }
+}
+
+
