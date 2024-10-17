d952dbd2ad04e1f3a2cae51c0cf89b66667d9dd7
Luiz Gustavo Chinelato Setten <luizgustavosetten@hotmail.com>
2024-10-17 15:13:23 -0300

    feat: list games done

--- routes/games/list.ts
+++ routes/games/list.ts
@@ -1,5 +1,5 @@
-import { DynamoDBClient, ScanCommand } from "@aws-sdk/client-dynamodb";
-import { DynamoDBDocumentClient } from "@aws-sdk/lib-dynamodb";
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import { DynamoDBDocumentClient, ScanCommand } from "@aws-sdk/lib-dynamodb";
 import { Handler } from "aws-lambda";
 
 const GAMES_TABLE = process.env.GAMES_TABLE;
