c489da1a414bd484108e2c434a281f1d835bc9cc
Luiz Gustavo Chinelato Setten <luizgustavosetten@hotmail.com>
2024-10-21 22:51:37 -0300

    refactor: formatting using biome

--- routes/games/list.ts
+++ routes/games/list.ts
@@ -1,24 +1,23 @@
 import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
 import { DynamoDBDocumentClient, ScanCommand } from "@aws-sdk/lib-dynamodb";
-import { Handler } from "aws-lambda";
+import type { Handler } from "aws-lambda";
 
 const GAMES_TABLE = process.env.GAMES_TABLE;
 const client = new DynamoDBClient();
 const docClient = DynamoDBDocumentClient.from(client);
 
 export const listGamesLambda: Handler = async (event, context, callback) => {
-  const params = {
-    TableName: GAMES_TABLE,
-  };
+	const params = {
+		TableName: GAMES_TABLE,
+	};
 
-  try {
-    const command = new ScanCommand(params);
-    const { Items } = await docClient.send(command);
+	try {
+		const command = new ScanCommand(params);
+		const { Items } = await docClient.send(command);
 
-    callback(null, Items)
-
-  } catch (error) {
-    console.log(error);
-    callback("Could not retrieve items")
-  }
-}
+		callback(null, Items);
+	} catch (error) {
+		console.log(error);
+		callback("Could not retrieve items");
+	}
+};
